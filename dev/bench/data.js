44e98d0c64b4862d1c619251f1021c3abdb7f6b8
github-action-benchmark <github@users.noreply.github.com>
2026-05-31 09:31:28 +0000

    add Go HNSW Benchmarks (go) benchmark result for f4dd80444816fe9a776b01dabb034bf3133b8034

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780219750781,
+  "lastUpdate": 1780219888161,
   "repoUrl": "https://github.com/Tejas242/sift",
   "entries": {
     "Go HNSW Benchmarks": [
@@ -72,6 +72,42 @@ window.BENCHMARK_DATA = {
             "value": 370580,
             "unit": "ns/op",
             "extra": "3260 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tmahajan09876@gmail.com",
+            "name": "Tejas Mahajan",
+            "username": "Tejas242"
+          },
+          "committer": {
+            "email": "tmahajan09876@gmail.com",
+            "name": "Tejas Mahajan",
+            "username": "Tejas242"
+          },
+          "distinct": true,
+          "id": "f4dd80444816fe9a776b01dabb034bf3133b8034",
+          "message": "docs: update README",
+          "timestamp": "2026-05-31T15:00:30+05:30",
+          "tree_id": "ac9c09223ef1c32b0e5c3dbc17165760595421a4",
+          "url": "https://github.com/Tejas242/sift/commit/f4dd80444816fe9a776b01dabb034bf3133b8034"
+        },
+        "date": 1780219887893,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHNSWInsert",
+            "value": 1711228,
+            "unit": "ns/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHNSWSearch",
+            "value": 369844,
+            "unit": "ns/op",
+            "extra": "3244 times\n4 procs"
           }
         ]
       }
