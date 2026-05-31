b01701a2bb757c97c9023782d050460f0bf77607
github-action-benchmark <github@users.noreply.github.com>
2026-05-31 09:09:52 +0000

    add Go HNSW Benchmarks (go) benchmark result for 383e11bcedfb1e5fe9804b76b991c709d5f9ce70

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1780218592973,
+  "repoUrl": "https://github.com/Tejas242/sift",
+  "entries": {
+    "Go HNSW Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "tmahajan09876@gmail.com",
+            "name": "Tejas Mahajan",
+            "username": "Tejas242"
+          },
+          "committer": {
+            "email": "tmahajan09876@gmail.com",
+            "name": "Tejas Mahajan",
+            "username": "Tejas242"
+          },
+          "distinct": true,
+          "id": "383e11bcedfb1e5fe9804b76b991c709d5f9ce70",
+          "message": "chore: download libtokenizers and auto-bootstrap gh-pages in CI workflows",
+          "timestamp": "2026-05-31T14:37:51+05:30",
+          "tree_id": "f68570749173e864c8077ea1fa20d9154fe07884",
+          "url": "https://github.com/Tejas242/sift/commit/383e11bcedfb1e5fe9804b76b991c709d5f9ce70"
+        },
+        "date": 1780218592330,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHNSWInsert",
+            "value": 1516152,
+            "unit": "ns/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkHNSWSearch",
+            "value": 338968,
+            "unit": "ns/op",
+            "extra": "3399 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
